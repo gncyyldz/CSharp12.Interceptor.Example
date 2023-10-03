12c555a4a82df61f90d23864fb35bd7b2b1c97f6
Gençay Yıldız <gyildizmail@gmail.com>
2023-10-04 02:02:57 +0300

    Add project files.

---
+++ Interceptor.Example/CodeGenerator.cs
@@ -0,0 +1,30 @@
+﻿using Amazon.S3.Model;
+using System.Runtime.CompilerServices;
+
+namespace Interceptor.Example
+{
+    static public class CodeGenerator
+    {
+        [InterceptsLocation(@"C:\Users\Gencay\Desktop\Interceptor.Example\Interceptor.Example\Program.cs", line: 3, column: 1)]
+        [InterceptsLocation(@"C:\Users\Gencay\Desktop\Interceptor.Example\Interceptor.Example\Program.cs", line: 4, column: 1)]
+        static public void XInterceptor()
+        {
+            Console.WriteLine("Doldur be meyhaneci, boş kalmasın kadehim...");
+        }
+
+        [InterceptsLocation(@"C:\Users\Gencay\Desktop\Interceptor.Example\Interceptor.Example\Program.cs", line: 5, column: 1)]
+        static public void ZInterceptor(int zParam)
+        {
+            Console.WriteLine(zParam * 3);
+        }
+
+        [InterceptsLocation(@"C:\Users\Gencay\Desktop\Interceptor.Example\Interceptor.Example\Program.cs", line: 8, column: 16)]
+        static public async Task<ListBucketsResponse> ListBucketsAsyncInterceptor(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            Console.WriteLine("List Buckets Async Interceptor");
+            return null;
+        }
+
+
+    }
+}
---
+++ Interceptor.Example/Program.cs
@@ -0,0 +1,23 @@
+﻿using Amazon.S3;
+
+X();
+Y();
+Z(3);
+
+AmazonS3Client s3Client = new();
+await s3Client.ListBucketsAsync();
+
+void X()
+{
+    Console.WriteLine("Laylaylom galiba sana göre sevmeler...");
+}
+
+void Y()
+{
+    Console.WriteLine("Sende başını alıp gitme ne olur, ne olur tut ellerimi...");
+}
+
+void Z(int z)
+{
+    Console.WriteLine(z);
+}
